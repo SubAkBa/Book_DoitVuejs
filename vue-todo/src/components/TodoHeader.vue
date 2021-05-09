406dca64f54eae2b7f384f9df56ad60e876ef8f2
SubAkBa <circle5926@gmail.com>
2021-05-09 20:18:52 +0900

    6-4-1

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,7 +1,7 @@
 <template lang="">
-    <div>
-        header
-    </div>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
 </template>
 <script>
 export default {
@@ -9,5 +9,9 @@ export default {
 }
 </script>
 <style lang="">
-    
+    h1 {
+        color: #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
